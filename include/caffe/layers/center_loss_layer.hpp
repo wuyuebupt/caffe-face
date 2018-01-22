11abb2ce65765809ceb4df215c5d7ca951f54509
wuyuebupt <wuyuebupt@gmail.com>
2018-01-22 17:30:55 -0500

    change center loss to center invariant loss and remove gpu version

--- include/caffe/layers/center_loss_layer.hpp
+++ include/caffe/layers/center_loss_layer.hpp
@@ -28,12 +28,12 @@ class CenterLossLayer : public LossLayer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   int M_;
   int K_;
@@ -41,6 +41,11 @@ class CenterLossLayer : public LossLayer<Dtype> {
   
   Blob<Dtype> distance_;
   Blob<Dtype> variation_sum_;
+
+  // Blob<Dtype> _sum_;
+  Blob<Dtype> lambda_;
+  Blob<Dtype> num_;
+  Dtype tau_;
 };
 
 }  // namespace caffe
--- src/caffe/layers/center_loss_layer.cpp
+++ src/caffe/layers/center_loss_layer.cpp
@@ -34,6 +34,28 @@ void CenterLossLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 
   }  // parameter initialization
   this->param_propagate_down_.resize(this->blobs_.size(), true);
+
+
+  // read 
+  //vector<int> mult_dims(1, bottom[0]->shape(1));
+  vector<int> mult_dims(1, N_);
+  num_.Reshape(mult_dims);
+  Dtype* num_data = num_.mutable_cpu_data();
+  const string& source = this->layer_param_.center_loss_param().source();
+  LOG(INFO) << "Opening file " << source;
+  std::ifstream infile(source.c_str());
+  string line;
+  size_t pos;
+  int num;
+  int label;
+  while (std::getline(infile, line)) {
+    pos = line.find_last_of(' ');
+    num = atoi(line.substr(pos + 1).c_str());
+    label = atoi(line.substr(0, pos).c_str());
+    caffe_set(1, Dtype(num), num_data + label);
+    LOG(INFO)<< label << " " << num;
+  }
+
 }
 
 template <typename Dtype>
@@ -48,7 +70,18 @@ void CenterLossLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   LossLayer<Dtype>::Reshape(bottom, top);
   distance_.ReshapeLike(*bottom[0]);
   variation_sum_.ReshapeLike(*this->blobs_[0]);
+
+
+  vector<int> lambda_dims = bottom[0]->shape();
+  lambda_dims[1] = 1;
+  lambda_.Reshape(lambda_dims);
 }
+
+
+// TODO
+// optimize computation cost for lambda
+// o(lambda) = min(#classes, #samples)
+
 
 template <typename Dtype>
 void CenterLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
@@ -57,15 +90,51 @@ void CenterLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* label = bottom[1]->cpu_data();
   const Dtype* center = this->blobs_[0]->cpu_data();
   Dtype* distance_data = distance_.mutable_cpu_data();
-  
+  Dtype* lambda_data = lambda_.mutable_cpu_data();
+
+  Dtype center_norm = Dtype(0);
+  // center norm 
+  for (int i = 0; i < N_; i++) {
+    // Dtype class_i_norm = sqrt(caffe_cpu_dot(K_, center + i * K_, center + i * K_));
+    Dtype class_i_norm = caffe_cpu_dot(K_, center + i * K_, center + i * K_);
+    // std::cout << "cneter_norm_" << i << " " << class_i_norm << std::endl; 
+    
+
+    // mean
+    center_norm += class_i_norm / N_ ;
+    // center_norm += caffe_cpu_dot(K_, center + i * K_, center + i * K_) / N_ ;
+
+    // max
+    // if (center_norm < class_i_norm ){
+    //	center_norm = class_i_norm;
+    // }
+  }
+  tau_ = center_norm;
+  std::cout << "cneter_norm " << center_norm << std::endl;
+
   // the i-th distance_data
   for (int i = 0; i < M_; i++) {
     const int label_value = static_cast<int>(label[i]);
     // D(i,:) = X(i,:) - C(y(i),:)
     caffe_sub(K_, bottom_data + i * K_, center + label_value * K_, distance_data + i * K_);
+    // 
+    // Dtype lambda = sqrt(caffe_cpu_dot(K_, center + label_value * K_, center + label_value * K_));
+    Dtype lambda = caffe_cpu_dot(K_, center + label_value * K_, center + label_value * K_);
+    lambda_data[i]  =  lambda - tau_;
+    // lambda_data[i]  = lambda / center_norm - 1;
+    // lambda_data[i]  =  1 - lambda/center_norm;
+    // lambda_data[i]  =   center_norm/lambda - 1;
+    // if (label_value < 5){
+    //   std::cout << "lambda " << lambda <<  " " << tau_ << " "<< lambda_data[i] <<std::endl;
+    // }
+    // std::cout << "lambda " << lambda << std::endl;
   }
-  Dtype dot = caffe_cpu_dot(M_ * K_, distance_.cpu_data(), distance_.cpu_data());
-  Dtype loss = dot / M_ / Dtype(2);
+
+
+  // Dtype dot = caffe_cpu_dot(M_ * K_, distance_.cpu_data(), distance_.cpu_data());
+  Dtype dot = caffe_cpu_dot(M_, lambda_data, lambda_data);
+  // Dtype dot = caffe_cpu_dot(M_, bottom_data, bottom_data);
+  Dtype loss = dot / M_ / Dtype(4);
   top[0]->mutable_cpu_data()[0] = loss;
 }
 
@@ -73,9 +142,14 @@ template <typename Dtype>
 void CenterLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
+
+  // const Dtype* bottom_data = bottom[0]->cpu_data();
+  const Dtype* lambda_data = lambda_.mutable_cpu_data();
+    const Dtype* label = bottom[1]->cpu_data();
+  const Dtype* center = this->blobs_[0]->cpu_data();
+const Dtype* num_data = num_.mutable_cpu_data();
   // Gradient with respect to centers
   if (this->param_propagate_down_[0]) {
-    const Dtype* label = bottom[1]->cpu_data();
     Dtype* center_diff = this->blobs_[0]->mutable_cpu_diff();
     Dtype* variation_sum_data = variation_sum_.mutable_cpu_data();
     const Dtype* distance_data = distance_.cpu_data();
@@ -96,8 +170,27 @@ void CenterLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   }
   // Gradient with respect to bottom data 
   if (propagate_down[0]) {
-    caffe_copy(M_ * K_, distance_.cpu_data(), bottom[0]->mutable_cpu_diff());
-    caffe_scal(M_ * K_, top[0]->cpu_diff()[0] / M_, bottom[0]->mutable_cpu_diff());
+    Dtype* bottom_diff_data = bottom[0]->mutable_cpu_diff();
+    for (int i = 0; i < M_; i++){
+	// Dtype scale = (Dtype(1) - lambda_data[i]) * (Dtype(1) - lambda_data[i]);
+	// Dtype scale =  Dtype(-1) + lambda_data[i];
+        const int label_value = static_cast<int>(label[i]);
+	Dtype scale = Dtype(0);
+        // if (Dtype(1)  > lambda_data[i]){
+	// scale =  lambda_data[i]  / num_data[label_value] ;
+	// scale =  lambda_data[i] / tau_ / num_data[label_value] * ((Dtype)1. - (Dtype)1. / N_);
+	scale =  lambda_data[i] * ((Dtype)1. - (Dtype)1. / N_) / num_data[label_value] ;
+	// }
+	
+	caffe_axpy(K_, scale, center + label_value * K_, bottom_diff_data + i * K_);
+	// caffe_axpy(K_, scale, bottom_data + i * K_, bottom_diff_data + i * K_);
+    }
+// std::cout << top[0]->cpu_diff()[0] << std::endl;
+    caffe_scal(M_ * K_, top[0]->cpu_diff()[0] / M_, bottom_diff_data);
+
+    // caffe_copy(M_ * K_, distance_.cpu_data(), bottom[0]->mutable_cpu_diff());
+    // caffe_copy(M_ * K_, distance_.cpu_data(), bottom[0]->mutable_cpu_diff());
+    // caffe_scal(M_ * K_, top[0]->cpu_diff()[0] / M_, bottom[0]->mutable_cpu_diff());
   }
   if (propagate_down[1]) {
     LOG(FATAL) << this->type()
